a53d232970d66b1aa806be88352c180bd9433ba2
Acosta Federico <federicoacosta002@gmail.com>
2023-11-29 17:34:22 -0300

    add boats images and visual fixes

--- battleship-game/src/components/ShipSelectionMenu/ShipSelectionMenu.jsx
+++ battleship-game/src/components/ShipSelectionMenu/ShipSelectionMenu.jsx
@@ -1,27 +1,39 @@
-// ShipSelectionMenu.js
-
-import React, { useState } from 'react';
+import { useState } from 'react';
 import './ShipSelectionMenu.css';
+
+import battleshipCarrierImage from '../../assets/battleship-carrier-image.webp';
+import battleshipCruiserImage from '../../assets/battleship-cruiser-image.jpg';
+import battleshipSubmarineImage from '../../assets/battleship-submarine-image.png';
+import battleshipBoatImage from '../../assets/battleship-boat-image.jpeg';
 
 export const ShipSelectionMenu = ({ onSelect }) => {
   const [selectedShip, setSelectedShip] = useState(null);
 
-  const shipTypes = ['carrier', 'cruiser', 'submarine', 'boat'];
+  const shipTypes = [
+    { type: 'Carrier', image: battleshipCarrierImage },
+    { type: 'Cruiser', image: battleshipCruiserImage },
+    { type: 'Submarine', image: battleshipSubmarineImage },
+    { type: 'Boat', image: battleshipBoatImage },
+  ];
 
   return (
     <div className="ship-selection-menu">
       <h3>Select a Ship:</h3>
       <ul className="ship-list">
-        {shipTypes.map((shipType) => (
+        {shipTypes.map((ship) => (
           <li
-            key={shipType}
-            className={`ship-list-item ${selectedShip === shipType ? 'selected' : ''}`}
+            key={ship.type}
+            className={`ship-list-item ${selectedShip === ship.type ? 'selected' : ''}`}
             onClick={() => {
-              setSelectedShip(shipType);
-              onSelect(shipType);
+              setSelectedShip(ship.type);
+              onSelect(ship.type);
             }}
           >
-            {shipType}
+            <div
+              className="ship-image"
+              style={{ backgroundImage: `url(${ship.image})` }}
+            ></div>
+            {ship.type}
           </li>
         ))}
       </ul>
