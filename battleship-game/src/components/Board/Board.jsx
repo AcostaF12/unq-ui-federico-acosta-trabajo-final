64ef86b428cddb78bb901266767d461c08e0d93f
Acosta, Federico <107961278+AcostaF12@users.noreply.github.com>
2023-12-05 14:01:09 -0300

    styles and cells orientation

--- battleship-game/src/components/Board/Board.jsx
+++ battleship-game/src/components/Board/Board.jsx
@@ -1,20 +1,31 @@
-import { Cell } from '../Cell/Cell';
-import './Board.css';
+import { Cell } from "../Cell/Cell";
+import "./Board.css";
 
 export const Board = ({ cells, isMyBoard, onClick }) => {
+  const alphabet = "ABCDEFGHIJ";
+  const numberSequence = Array.from({ length: 10 }, (_, i) => i + 1);
+
   return (
-    <div className={`board ${isMyBoard ? 'my-board' : 'enemy-board'}`}>
-      {cells.map((row, rowIndex) => (
-        <div key={rowIndex} className="row">
-          {row.map((cell, colIndex) => (
-            <Cell
-              key={colIndex}
-              value={cells[rowIndex][colIndex] && cells[rowIndex][colIndex].value !== null ? cells[rowIndex][colIndex].value : "Empty"}
-              onClick={() => onClick(rowIndex, colIndex)}
-            />
-          ))}
-        </div>
-      ))}
+    <div>
+      <div className={`board ${isMyBoard ? "my-board" : "enemy-board"}`}>
+        {cells.map((row, rowIndex) => (
+          <div key={rowIndex} className="row">
+            <div className="number-cell">{alphabet[rowIndex]}</div>
+            {row.map((cell, colIndex) => (
+              <Cell
+                key={colIndex}
+                value={
+                  cells[rowIndex][colIndex] &&
+                  cells[rowIndex][colIndex].value !== null
+                    ? cells[rowIndex][colIndex].value
+                    : "Empty"
+                }
+                onClick={() => onClick(rowIndex, colIndex)}
+              />
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
